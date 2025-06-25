104e51237bb26a1d490ccbafdff17cd60f5778c4
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-06-25 20:27:54 -0300

    feat(Finca): se agregó todo el flujo para la entidad Finca.

--- src/main/java/com/example/fincas_grupo3/domain/ports/in/UsuarioInPort.java
+++
@@ -1,4 +0,0 @@
-package com.example.fincas_grupo3.domain.ports.in;
-
-public interface UsuarioInPort extends ActualizarUsuarioUseCase, EliminarUsuarioUseCase, ObtenerUsuarioUseCase, CrearUsuarioUseCase{
-}
---
+++ src/main/java/com/example/fincas_grupo3/infrastructure/Mappers/usuario/UsuarioMapper.java
@@ -0,0 +1,15 @@
+package com.example.fincas_grupo3.infrastructure.Mappers.usuario;
+
+
+import com.example.fincas_grupo3.domain.models.usuario.Usuario;
+import com.example.fincas_grupo3.infrastructure.entities.usuario.UsuarioEntidad;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring",implementationName = "usuarioMapperInfra")
+public abstract class UsuarioMapper {
+
+    public abstract Usuario toModel(UsuarioEntidad entidad);
+
+    public abstract UsuarioEntidad toEntity(Usuario usuario);
+
+}
