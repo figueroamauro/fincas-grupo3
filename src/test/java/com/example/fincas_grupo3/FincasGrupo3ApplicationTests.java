e712ad1328f3a8c8b136071ca318914940c113c7
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-06-25 13:31:12 -0300

    build: se creó la estructura base del proyecto Spring Boot.

---
+++ src/test/java/com/example/fincas_grupo3/FincasGrupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fincas_grupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincasGrupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
