4787eca9f72b75bfc91af6d1c42489535092d610
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-06-26 10:55:07 -0300

    feat(Foto): se agregó la entidad Fotos.

--- src/main/java/com/example/fincas_grupo3/application/usecases/provincia/EliminarProvinciaUseCaseImpl.java
+++ src/main/java/com/example/fincas_grupo3/application/usecases/provincia/EliminarProvinciaUseCaseImpl.java
@@ -2,7 +2,9 @@ package com.example.fincas_grupo3.application.usecases.provincia;
 
 import com.example.fincas_grupo3.domain.ports.in.provincia.EliminarProvinciaUseCase;
 import com.example.fincas_grupo3.domain.ports.out.provincia.ProvinciaOutPort;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EliminarProvinciaUseCaseImpl implements EliminarProvinciaUseCase {
     private final ProvinciaOutPort provinciaOutPort;
 
--- src/main/java/com/example/fincas_grupo3/application/usecases/provincia/ProvinciaUseCases.java
+++ src/main/java/com/example/fincas_grupo3/application/usecases/provincia/ProvinciaUseCases.java
@@ -1,40 +1,48 @@
 package com.example.fincas_grupo3.application.usecases.provincia;
 
 import com.example.fincas_grupo3.domain.models.provincia.Provincia;
-import com.example.fincas_grupo3.domain.ports.in.provincia.ProvinciaInPort;
+import com.example.fincas_grupo3.domain.ports.in.provincia.*;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProvinciaUseCases implements ProvinciaInPort {
 
-    private final ProvinciaInPort provinciaInPort;
+    private final CrearProvinciaUseCase crearProvinciaUseCase;
+    private final ActualizarProvinciaUseCase actualizarProvinciaUseCase;
+    private final EliminarProvinciaUseCase eliminarProvinciaUseCase;
+    private final ObtenerProvinciaUseCase obtenerProvinciaUseCase;
 
-    public ProvinciaUseCases(ProvinciaInPort provinciaInPort) {
-        this.provinciaInPort = provinciaInPort;
+    public ProvinciaUseCases(CrearProvinciaUseCase crearProvinciaUseCase, ActualizarProvinciaUseCase actualizarProvinciaUseCase, EliminarProvinciaUseCase eliminarProvinciaUseCase, ObtenerProvinciaUseCase obtenerProvinciaUseCase) {
+        this.crearProvinciaUseCase = crearProvinciaUseCase;
+        this.actualizarProvinciaUseCase = actualizarProvinciaUseCase;
+        this.eliminarProvinciaUseCase = eliminarProvinciaUseCase;
+        this.obtenerProvinciaUseCase = obtenerProvinciaUseCase;
     }
 
     @Override
     public Provincia actualizarProvincia(Provincia provincia) {
-        return provinciaInPort.actualizarProvincia(provincia);
+        return actualizarProvinciaUseCase.actualizarProvincia(provincia);
     }
 
     @Override
     public Provincia crearProvincia(Provincia provincia) {
-        return provinciaInPort.crearProvincia(provincia);
+        return crearProvinciaUseCase.crearProvincia(provincia);
     }
 
     @Override
     public Boolean eliminarProvinciaPorId(Long id) {
-        return provinciaInPort.eliminarProvinciaPorId(id);
+        return eliminarProvinciaUseCase.eliminarProvinciaPorId(id);
     }
 
     @Override
     public Provincia obtenerProvinciaPorId(Long id) {
-        return provinciaInPort.obtenerProvinciaPorId(id);
+        return obtenerProvinciaUseCase.obtenerProvinciaPorId(id);
     }
 
     @Override
     public List<Provincia> obtenerProvincias() {
-        return provinciaInPort.obtenerProvincias();
+        return obtenerProvinciaUseCase.obtenerProvincias();
     }
 }
