ca3309d7c39897a43e5191d5db4b58bc72e1b22d
Martin Alderete <malderete@gylgroup.local>
2025-06-26 09:27:24 -0300

    feat(Provincia): se agregó todo el flujo para la entidad Provincia

---
+++ src/main/java/com/example/fincas_grupo3/application/usecases/provincia/CrearProvinciaUseCaseImpl.java
@@ -0,0 +1,18 @@
+package com.example.fincas_grupo3.application.usecases.provincia;
+
+import com.example.fincas_grupo3.domain.models.provincia.Provincia;
+import com.example.fincas_grupo3.domain.ports.in.provincia.CrearProvinciaUseCase;
+import com.example.fincas_grupo3.domain.ports.out.provincia.ProvinciaOutPort;
+
+public class CrearProvinciaUseCaseImpl implements CrearProvinciaUseCase {
+    private final ProvinciaOutPort provinciaOutPort;
+
+    public CrearProvinciaUseCaseImpl(ProvinciaOutPort provinciaOutPort) {
+        this.provinciaOutPort = provinciaOutPort;
+    }
+
+    @Override
+    public Provincia crearProvincia(Provincia provincia) {
+        return provinciaOutPort.guardar(provincia);
+    }
+}
